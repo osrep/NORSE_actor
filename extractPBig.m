564788bee6db670d2c39f409fe1efa696c62cdd4
olaszsoma <olaszsoma@gmail.com>
2019-10-29 12:35:16 +0100

    Some comments modified.

--- extractPBig.m
+++ extractPBig.m
@@ -1,17 +1,14 @@
-%%% extractDistribution.m
+%%% extractPBig.m
 %%% 12/09/2019
 %%% Written by Soma Olasz
 %%% 
 %%% This script is created to extract the p coordinate values
 %%% from the last time step of a NORSE calculation to Python
 %%% languge.
-%%% 
-%%% 
 
-function double = extractDistribution(NORSEobject)
+function double = extractPBig(NORSEobject)
 
     % take the pBig coordinate values of the NORSE object
     double = NORSEobject.grid.pBig;
-    
 
 end
--- extractXiBig.m
+++ extractXiBig.m
@@ -1,17 +1,14 @@
-%%% extractDistribution.m
+%%% extractXiBig.m
 %%% 12/09/2019
 %%% Written by Soma Olasz
 %%% 
 %%% This script is created to extract the Xi coordinate values
 %%% from the last time step of a NORSE calculation to Python
 %%% languge.
-%%% 
-%%% 
 
-function double = extractDistribution(NORSEobject)
+function double = extractXiBig(NORSEobject)
 
     % take the XiBig coordinate values of the NORSE object
     double = NORSEobject.grid.xiBig;
-    
 
 end
